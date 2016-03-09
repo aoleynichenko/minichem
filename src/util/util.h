9c01d362ee2e78b7dd8d2f9c4643c652a9df1995
aoleynichenko <ao2310@yandex.ru>
2016-03-09 19:03:00 +0000

    Create util.h

---
+++ src/util/util.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stddef.h>
+
+int buildno();
+void echo(char *filename);
+void errquit(char *errmessage);
+void help();
+void line_separator();
+void print_header();
+
+/* memory management */
+void setmemavail(int bytes);
+void *qalloc(size_t bytes);
+void qfree(void *p, size_t n);
+void memstats();
