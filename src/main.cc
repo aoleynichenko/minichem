a7be9f7bb10b710b80c0616713cdfd2b7aeb1690
aoleynichenko <ao2310@yandex.ru>
2016-10-16 12:41:37 +0000

    new makefiles tree

--- src/main.cc
+++ src/main.cc
@@ -9,14 +9,18 @@
  *  Invokes all another modules: input, hf, mp2, cc, geom, etc.
  */
 
+#include <ctime>
+
 #include "minichem.h"
-#include "OutputStream.h"
+#include "lib/io/OutputStream.h"
 
 using namespace minichem;
 
 void outputHeader()
 {
+	time_t t = time(0);
 	OutputStream* out = OutputStream::getStdout();
+
 	out->println();
 	out->printf("                           **********************\n");
 	out->printf("                           *                    *\n");
@@ -26,7 +30,15 @@ void outputHeader()
 	out->println();
 	out->printf("Minichem is an open-source simple quantum chemistry code, \
 written in educational purposes\n");
-	out->printf("Built: %s %s\n", MINICHEM_BUILD_DATE, MINICHEM_BUILD_TIME);
+	out->printf("Build date: %s %s\n", MINICHEM_BUILD_DATE, MINICHEM_BUILD_TIME);
+	#if defined __ICC
+	out->printf("Compiler:   Intel C Compiler %d\n", __ICC);
+	#elif defined __GNUC__
+	out->printf("Compiler:   gcc %d.%d.%d\n", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+	#else
+	out->printf("Compiler:   undetected\n");
+	#endif
+	printf("Date:       %s", asctime(localtime(&t)));
 	out->printf("Authors:\n");
 	out->printf("  Alexander Oleynichenko        alexvoleynichenko@gmail.com\n");
 	out->println();
