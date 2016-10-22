d40fffdcc8a0e184ada8d826ead2db489f0b0eff
aoleynichenko <ao2310@yandex.ru>
2016-10-22 16:22:02 +0000

    manual for basis sets, started class basisset

---
+++ src/lib/basis/BasisSet.cc
@@ -0,0 +1,7 @@
+#include "BasisSet.h"
+
+namespace minichem {
+
+
+
+} //namespace minichem
---
+++ src/lib/basis/BasisSet.h
@@ -0,0 +1,11 @@
+#ifndef _BASIS_SET_H_INCLUDED
+#define _BASIS_SET_H_INCLUDED
+
+namespace minichem {
+
+class BasisSet {
+};
+
+} // namespace minichem
+
+#endif // _BASIS_SET_H_INCLUDED
